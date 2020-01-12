f4cb82b76f88d6ba7be1ada1818b2ae88f51911b
Ian Wilson <ianwilson1337@gmail.com>
2020-01-12 17:06:34 -0600

    Beautified text in file.

--- server/lobby.js
+++ server/lobby.js
@@ -2,21 +2,21 @@ var express = require('express');
 
 
 module.exports = {
-  initialPort:8100,
-  currentPort:8100,
-  maximumPort:8200,
-  reservedPorts:[],
-  listeners:[],
+  initialPort: 8100,
+  currentPort: 8100,
+  maximumPort: 8200,
+  reservedPorts: [],
+  listeners: [],
   defaultTimeout: 1000000,
-  getPort:function(){
+  getPort: function() {
     this.currentPort++;
 
-    if(this.currentPort>this.maximumPort){
+    if (this.currentPort > this.maximumPort) {
       //Reset when the number gets to high
       this.currentPort = this.initialPort;
     }
 
-    while(this.reservedPorts.indexOf(this.currentPort)!==-1){
+    while (this.reservedPorts.indexOf(this.currentPort) !== -1) {
       //Skip ports that have been blocked
       this.currentPort++;
     };
@@ -30,34 +30,34 @@ module.exports = {
       currPort = this.getPort(),
       defaultTimeout = this.defaultTimeout;
 
-    function countUsers(){
-      var currUsers  = 0;
-      expressWs.getWss().clients.forEach(client=> (currUsers++));
+    function countUsers() {
+      var currUsers = 0;
+      expressWs.getWss().clients.forEach(client => (currUsers++));
 
       return currUsers;
     }
 
-    function closeRoom(interval){
-      expressWs.getWss().clients.forEach(client=> {
+    function closeRoom(interval) {
+      expressWs.getWss().clients.forEach(client => {
         client.send("Closing connection, timeout lapsed.");
         client.close();
       });
       currentListener.close();
-      var roomIndex=false;
-      openRooms.some((room, indx) =>{
-        if(room.roomCode===roomCode){
+      var roomIndex = false;
+      openRooms.some((room, indx) => {
+        if (room.roomCode === roomCode) {
           roomIndex = indx;
           return true;
         }
       });
-      if(roomIndex!==false){
+      if (roomIndex !== false) {
         openRooms.splice(roomIndex, 1);
       }
 
       console.log("Closed Room: " + roomCode);
     }
 
-    function resetTimeout(){
+    function resetTimeout() {
       timeout = new Date().getTime();
     }
 
@@ -73,13 +73,13 @@ module.exports = {
 
     });
 
-    console.log('Room ' + roomCode +" created on port "+ currPort);
+    console.log('Room ' + roomCode + " created on port " + currPort);
     var currentListener = lobbyApp.listen(currPort);
     this.listeners.push(currentListener);
 
     //Close the server after it has been idle for too long
-    setInterval(function(){
-      if((new Date().getTime() - timeout) > defaultTimeout){
+    setInterval(function() {
+      if ((new Date().getTime() - timeout) > defaultTimeout) {
         //Server has been idle for too long, shut it down
         closeRoom(this);
         clearInterval(this);
@@ -87,8 +87,8 @@ module.exports = {
     }, 1000);
 
     var currentRoom = {
-      port:currPort,
-      roomCode:roomCode
+      port: currPort,
+      roomCode: roomCode
     };
     openRooms.push(currentRoom);
     return currentRoom;
