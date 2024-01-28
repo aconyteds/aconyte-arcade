276682dcc10f7999a9f5cff533a5e2301af679fc
Ian Wilson <ianwilson1337@gmail.com>
2024-01-28 11:04:50 -0600

    Added tests to validate the game engine is working properly

---
+++ common/components/LogicGame/engine.test.ts
@@ -0,0 +1,160 @@
+import { checkForDefeat, generateGame } from "./engine";
+import {
+  DIFFICULTY_CONFIGURATIONS,
+  Difficulty,
+  ItemContainerProps,
+  ItemType,
+} from "./models";
+
+describe("generateGame", () => {
+  it.each([[Difficulty.Easy], [Difficulty.Medium], [Difficulty.Hard]])(
+    "should generate a valid game for difficulty %s",
+    (difficulty: Difficulty) => {
+      const game = generateGame({ difficulty });
+      const {
+        additionalContainers,
+        containerLimit,
+        containers,
+        uniqueItemTypeCount,
+      } =
+        DIFFICULTY_CONFIGURATIONS[
+          difficulty === "easy"
+            ? Difficulty.Easy
+            : difficulty === "medium"
+            ? Difficulty.Medium
+            : Difficulty.Hard
+        ];
+      // Check if the game has the correct properties
+      expect(game).toHaveProperty("containers");
+      expect(game).toHaveProperty("additionalContainers");
+      expect(game).toHaveProperty("containerLimit");
+      expect(game).toHaveProperty("uniqueItemTypeCount");
+
+      // Check if the game has the correct values
+      expect(game.additionalContainers).toBe(additionalContainers);
+      expect(game.containerLimit).toBe(containerLimit);
+      expect(game.uniqueItemTypeCount).toBe(uniqueItemTypeCount);
+      expect(game.containers.length).toBe(containers);
+
+      let totalItems = 0;
+      let uniqueItems = new Set<ItemType>();
+      game.containers.forEach((container) => {
+        expect(container.items.length).toBeLessThanOrEqual(containerLimit);
+        totalItems += container.items.length;
+        container.items.forEach((item) => uniqueItems.add(item));
+      });
+
+      expect(totalItems).toBe(containerLimit * uniqueItemTypeCount);
+      expect(uniqueItems.size).toBe(uniqueItemTypeCount);
+    }
+  );
+
+  it.each([
+    [
+      true,
+      [
+        {
+          indx: 0,
+          items: ["otter"],
+        },
+        {
+          indx: 1,
+          items: ["fish"],
+        },
+      ] as ItemContainerProps[],
+      1,
+    ],
+    [
+      false,
+      [
+        {
+          indx: 0,
+          items: ["otter"],
+        },
+        {
+          indx: 1,
+          items: ["otter"],
+        },
+      ] as ItemContainerProps[],
+      2,
+    ],
+    [
+      false,
+      [
+        {
+          indx: 0,
+          items: [],
+        },
+        {
+          indx: 1,
+          items: ["otter"],
+        },
+      ] as ItemContainerProps[],
+      2,
+    ],
+    [
+      false,
+      [
+        {
+          indx: 0,
+          items: ["otter"],
+        },
+        {
+          indx: 1,
+          items: ["atom", "otter"],
+        },
+      ] as ItemContainerProps[],
+      2,
+    ],
+    [
+      true,
+      [
+        {
+          indx: 0,
+          items: ["otter"],
+        },
+        {
+          indx: 1,
+          items: ["fish", "dragon"],
+        },
+        {
+          indx: 2,
+          items: ["radiation"],
+        },
+      ] as ItemContainerProps[],
+      2,
+    ],
+    [
+      true,
+      [
+        {
+          indx: 0,
+          items: ["otter"],
+        },
+        {
+          indx: 1,
+          items: ["otter"],
+        },
+        {
+          indx: 2,
+          items: ["otter"],
+        },
+        {
+          indx: 3,
+          items: ["otter"],
+        },
+      ] as ItemContainerProps[],
+      1,
+    ],
+  ])(
+    "checkForDefeat should return %s based on the provided game input",
+    (
+      expectedValue: boolean,
+      game: ItemContainerProps[],
+      containerLimit: number
+    ) => {
+      const result = checkForDefeat(game, containerLimit);
+      expect(result).toBe(expectedValue);
+    }
+  );
+});
